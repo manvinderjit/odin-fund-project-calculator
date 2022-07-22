3726fba8172672e48e8635e98e8b0ea2b1affbec
manvinderjit <manvinderjit@gmail.com>
2022-07-22 19:31:25 -0400

    Step 6 Main Exercises Completed

--- js-script.js
+++ js-script.js
@@ -1,7 +1,7 @@
 let displayData = "";
-
-let displayVariable;
-let newVariable;
+let operatorChosen = "";
+let firstNumber = null;
+let operationExecuted = false;
 
 const add = function(a, b) {
 	return a+b;
@@ -16,22 +16,25 @@ const multiply = function(a, b) {
 };
 
 const divide = function (a, b) {
-    return a / b;
+    return Math.round((a/b) * 10000) / 10000;
 }
 
 const operate = function (operator, a, b) {
     if(operator === "+"){
-        return add(a, b);
+        return add(Number(a), Number(b));
 
     } else if (operator === "-") {
-        return subtract(a, b);
+        return subtract(Number(a), Number(b));
 
     } else if (operator === "*") {
-        return multiply(a, b);
+        return multiply(Number(a), Number(b));
 
-    } else if (operator === "/"){
-        return divide(a, b);
-    }
+    } else if (operator === "/"){           
+        if(Number(b) == 0){                       
+            return null
+        }
+        return divide(Number(a), Number(b));
+    } 
 }
 
 const divDisplay = document.querySelector("#calc-display");
@@ -47,13 +50,65 @@ const populateDisplay = function (newDisplayData) {
 
 const numberButtons = document.querySelectorAll(".number-button");
 numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click', () => {        
-        populateDisplay(numberButton.id);
+    numberButton.addEventListener('click', () => {    
+        if(operationExecuted === true){
+            displayData = "";
+            operationExecuted = false;
+        }
+        populateDisplay(numberButton.id);           
     })
 })
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener('click', () => {
-    displayData = "";
+    clearEverything();
+})
+
+const clearEverything = function (){
+    displayData = "0";
     populateDisplay('');
+    firstNumber = null;
+    operatorChosen = "";
+}
+
+const operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {  
+        if(operatorChosen == "/" && Number(displayData) == 0 ){
+            alert("Can not divide by zero!");
+            clearEverything();
+            return
+        }
+        if(firstNumber !== null){                            
+            displayData = operate(operatorChosen, firstNumber, displayData);            
+            operatorChosen = (operatorButton.id);            
+            firstNumber = displayData;                        
+            populateDisplay('');
+            displayData = "";          
+            
+        }else{            
+            firstNumber = displayData;
+            operatorChosen = (operatorButton.id);
+            displayData = "";
+        }        
+    })
+})
+
+const executionButton = document.querySelector("#equal-to");
+executionButton.addEventListener('click' , () => {        
+    if(firstNumber === null || displayData == ""){
+        return
+    }
+    let temp = operate(operatorChosen, firstNumber, displayData);
+    if(temp === null){
+        alert("Can't divide by zero!");
+        clearEverything();
+        return
+    } else{
+        displayData = temp
+        firstNumber = null;
+        populateDisplay('');     
+        operationExecuted = true;   
+    }
+    
 })
